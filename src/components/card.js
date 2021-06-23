a98573872d3262bf9b744bebe7fe089551eff8bc
josiahmokob0 <jmnyarega@gmail.com>
2021-06-23 19:15:48 +0300

    add quote

--- src/components/card.js
+++ src/components/card.js
@@ -3,11 +3,11 @@ import Title from "./title";
 import Paragraph from "./text";
 
 const Card = ({ name, status, img_url, title, text, className }) => (
-  <div className={`card ${className}`}>
+  <article className={`card ${className}`}>
     <Header name={name} status={status} img_url={img_url} />
     <Title text={title} />
     <Paragraph text={text} />
-  </div>
+  </article>
 );
 
 export default Card;
